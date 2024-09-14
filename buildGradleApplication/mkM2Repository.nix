f6f468d02d7ec0e46fdbaaac7f77465130b4bd8b
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-09-14 13:05:45 +0200

    jhasdkljhasd

--- buildGradleApplication/mkM2Repository.nix
+++ buildGradleApplication/mkM2Repository.nix
@@ -11,16 +11,11 @@
   repositories ? ["https://plugins.gradle.org/m2/" "https://repo1.maven.org/maven2/"],
   verificationFile ? "gradle/verification-metadata.xml",
 }: let
-  filteredSrc = lib.fileset.toSource {
-    root = src;
-    fileset = lib.path.append src verificationFile;
-  };
-
   depSpecs = builtins.filter dependencyFilter (
     # Read all build and runtime dependencies from the verification-metadata XML
     builtins.fromJSON (builtins.readFile (
       runCommandNoCC "depSpecs" {buildInputs = [python3];}
-      "python ${./parse.py} ${filteredSrc}/${verificationFile} ${builtins.toString (builtins.map lib.escapeShellArg repositories)}> $out"
+      "python ${./parse.py} ${src}/${verificationFile} ${builtins.toString (builtins.map lib.escapeShellArg repositories)}> $out"
     ))
   );
   mkDep = depSpec: {
